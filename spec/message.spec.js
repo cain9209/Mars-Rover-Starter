3290a0ffaf6806c6c0af30989e37b8d1e248d71d
Zack Cain <zacharylcain@gmail.com>
2024-02-20 11:03:41 -0600

    test complete for part two

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -22,7 +22,11 @@ describe("Message class", function() {
 
 describe("Message class", function() {
     it("contains a command array passed into the constructor as the 2nd argument", function(){
-        expect().tobe()
+        const testName = name1;
+        const testCommands = ['command1','command2'];
+        const message = new message(testName,testCommands);
+
+        expect(message).to.have.property('commands').that.deep.equals(testCommands);
     })
 
 });
