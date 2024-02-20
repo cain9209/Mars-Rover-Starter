3cdf9608e3cc54cba2deb82ebffebf860eef9978
Zack Cain <zacharylcain@gmail.com>
2024-02-20 14:34:51 -0600

    Message class is done along with test scripts

---
+++ extra.js
@@ -0,0 +1,7 @@
+let commands = [new Command('name2','command2'), new Command('name3','command3')]
+let message = new Message('Test message with two comands', commands)
+console.log(message.commands)
+
+
+
+//Just in case for message class//
--- message.js
+++ message.js
@@ -12,7 +12,5 @@ class Message {
       }
       }
    }
-let commands = [new Command('name2','command2'), new Command('name3','command3')]
-let message = new Message('Test message with two comands', commands)
-console.log(message.commands)
+
 module.exports = Message;
