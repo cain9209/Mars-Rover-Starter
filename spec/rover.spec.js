5a0e5ed4a9f014e7f203db30151aea7c44bcefa1
Zack Cain <zacharylcain@gmail.com>
2024-03-02 15:22:15 -0600

    changes to help pass more test

--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -15,6 +15,40 @@ expect(rover.position).toBe(10);
 expect(rover.mode).toBe('NORMAL');
 expect(rover.generatorWatts).toBe(110);
   });
+
+  it("response returned by receiveMessage contains the name of the message", () => {
+  const message = new Message("Test message name"); 
+    const rover = new Rover(98382); 
+    const response = rover.receiveMessage(message);
+    expect(response.message).toBe("Test message name");
+  });
+
+it ("response returned by receiveMessage includes two results if two commands are sent in the message", () => {
+  const commands = [
+    new Command("MODE_CHANGE","LOW_POWER"),
+    new Command("STATUS_CHECK"),
+  ];
+
+  const message = new Message("Test Message", commands);
+   const rover = new Rover(98382); 
+   const response = rover.receiveMessage(message);
+   expect(response.results.length).toEqual(commands.length);
+ });
+
+  it("responds correctly to the status check command", () => {
+    const commands = [new Command("STATUS_CHECK")];
+    const message = new Message("Test message name", commands);
+    const rover = new Rover(98382); 
+    const response = rover.receiveMessage(message);
+    
+    expect(response.results[0]).toHaveProperty("roverStatus");
+    expect(response.results[0].roverStatus.mode).toBe("NORMAL");
+    expect(response.results[0].roverStatus.generatorWatts).toEqual(110);
+    expect(response.results[0].roverStatus.position).toEqual(98382);
+  });
+
+
+
 });
 
 
